a0d6f01e68baf63f4395643f79daa62d5553d6b9
Bekzod <bekzodbaratov334@gmail.com>
2023-02-09 01:18:00 +0500

    change url

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,3 +1,3 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://orca-app-nn67b.ondigitalocean.app/api/v1/";
+axios.defaults.baseURL = "https://plankton-app-vomkh.ondigitalocean.app/api/v1/";
