9cbbcd408e53a79c60e86e5a403aede796f2ba94
Valisher Botirov <valisherbotirov1218@gmail.com>
2023-04-18 16:06:09 +0500

    fix: dashboar code

--- src/components/home/HomeComp.vue
+++ src/components/home/HomeComp.vue
@@ -6,30 +6,43 @@
       <div class="w-full px-6 sm:w-1/2 xl:w-1/3">
         <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
           <div class="p-3 rounded-full bg-indigo-600 bg-opacity-75">
-            <svg class="h-8 w-8 text-white" viewBox="0 0 28 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg
+              class="h-8 w-8 text-white"
+              viewBox="0 0 28 30"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path
                 d="M18.2 9.08889C18.2 11.5373 16.3196 13.5222 14 13.5222C11.6804 13.5222 9.79999 11.5373 9.79999 9.08889C9.79999 6.64043 11.6804 4.65556 14 4.65556C16.3196 4.65556 18.2 6.64043 18.2 9.08889Z"
-                fill="currentColor" />
+                fill="currentColor"
+              />
               <path
                 d="M25.2 12.0444C25.2 13.6768 23.9464 15 22.4 15C20.8536 15 19.6 13.6768 19.6 12.0444C19.6 10.4121 20.8536 9.08889 22.4 9.08889C23.9464 9.08889 25.2 10.4121 25.2 12.0444Z"
-                fill="currentColor" />
+                fill="currentColor"
+              />
               <path
                 d="M19.6 22.3889C19.6 19.1243 17.0927 16.4778 14 16.4778C10.9072 16.4778 8.39999 19.1243 8.39999 22.3889V26.8222H19.6V22.3889Z"
-                fill="currentColor" />
+                fill="currentColor"
+              />
               <path
                 d="M8.39999 12.0444C8.39999 13.6768 7.14639 15 5.59999 15C4.05359 15 2.79999 13.6768 2.79999 12.0444C2.79999 10.4121 4.05359 9.08889 5.59999 9.08889C7.14639 9.08889 8.39999 10.4121 8.39999 12.0444Z"
-                fill="currentColor" />
+                fill="currentColor"
+              />
               <path
                 d="M22.4 26.8222V22.3889C22.4 20.8312 22.0195 19.3671 21.351 18.0949C21.6863 18.0039 22.0378 17.9556 22.4 17.9556C24.7197 17.9556 26.6 19.9404 26.6 22.3889V26.8222H22.4Z"
-                fill="currentColor" />
+                fill="currentColor"
+              />
               <path
                 d="M6.64896 18.0949C5.98058 19.3671 5.59999 20.8312 5.59999 22.3889V26.8222H1.39999V22.3889C1.39999 19.9404 3.2804 17.9556 5.59999 17.9556C5.96219 17.9556 6.31367 18.0039 6.64896 18.0949Z"
-                fill="currentColor" />
+                fill="currentColor"
+              />
             </svg>
           </div>
 
           <div class="mx-5">
-            <h4 class="text-2xl font-semibold text-gray-700">{{ users?.length }}</h4>
+            <h4 class="text-2xl font-semibold text-gray-700">
+              {{ users?.length }}
+            </h4>
             <div class="text-gray-500">Obunachilar</div>
           </div>
         </div>
@@ -38,16 +51,24 @@
       <div class="w-full mt-6 px-6 sm:w-1/2 xl:w-1/3 sm:mt-0">
         <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
           <div class="p-3 rounded-full bg-orange-600 bg-opacity-75">
-            <svg class="h-8 w-8 text-white" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg
+              class="h-8 w-8 text-white"
+              viewBox="0 0 28 28"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path
                 d="M4.19999 1.4C3.4268 1.4 2.79999 2.02681 2.79999 2.8C2.79999 3.57319 3.4268 4.2 4.19999 4.2H5.9069L6.33468 5.91114C6.33917 5.93092 6.34409 5.95055 6.34941 5.97001L8.24953 13.5705L6.99992 14.8201C5.23602 16.584 6.48528 19.6 8.97981 19.6H21C21.7731 19.6 22.4 18.9732 22.4 18.2C22.4 17.4268 21.7731 16.8 21 16.8H8.97983L10.3798 15.4H19.6C20.1303 15.4 20.615 15.1004 20.8521 14.6261L25.0521 6.22609C25.2691 5.79212 25.246 5.27673 24.991 4.86398C24.7357 4.45123 24.2852 4.2 23.8 4.2H8.79308L8.35818 2.46044C8.20238 1.83722 7.64241 1.4 6.99999 1.4H4.19999Z"
-                fill="currentColor" />
+                fill="currentColor"
+              />
               <path
                 d="M22.4 23.1C22.4 24.2598 21.4598 25.2 20.3 25.2C19.1403 25.2 18.2 24.2598 18.2 23.1C18.2 21.9402 19.1403 21 20.3 21C21.4598 21 22.4 21.9402 22.4 23.1Z"
-                fill="currentColor" />
+                fill="currentColor"
+              />
               <path
                 d="M9.1 25.2C10.2598 25.2 11.2 24.2598 11.2 23.1C11.2 21.9402 10.2598 21 9.1 21C7.9402 21 7 21.9402 7 23.1C7 24.2598 7.9402 25.2 9.1 25.2Z"
-                fill="currentColor" />
+                fill="currentColor"
+              />
             </svg>
           </div>
 
@@ -61,17 +82,31 @@
       <div class="w-full mt-6 px-6 sm:w-1/2 xl:w-1/3 xl:mt-0">
         <div class="flex items-center px-5 py-6 shadow-sm rounded-md bg-white">
           <div class="p-3 rounded-full bg-pink-600 bg-opacity-75">
-            <svg class="h-8 w-8 text-white" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M6.99998 11.2H21L22.4 23.8H5.59998L6.99998 11.2Z" fill="currentColor" stroke="currentColor"
-                stroke-width="2" stroke-linejoin="round" />
+            <svg
+              class="h-8 w-8 text-white"
+              viewBox="0 0 28 28"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6.99998 11.2H21L22.4 23.8H5.59998L6.99998 11.2Z"
+                fill="currentColor"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linejoin="round"
+              />
               <path
                 d="M9.79999 8.4C9.79999 6.08041 11.6804 4.2 14 4.2C16.3196 4.2 18.2 6.08041 18.2 8.4V12.6C18.2 14.9197 16.3196 16.8 14 16.8C11.6804 16.8 9.79999 14.9197 9.79999 12.6V8.4Z"
-                stroke="currentColor" stroke-width="2" />
+                stroke="currentColor"
+                stroke-width="2"
+              />
             </svg>
           </div>
 
           <div class="mx-5">
-            <h4 class="text-2xl font-semibold text-gray-700">215,542</h4>
+            <h4 class="text-2xl font-semibold text-gray-700">
+              {{ productsList.length }}
+            </h4>
             <div class="text-gray-500">Mavjud mahsulotlar</div>
           </div>
         </div>
@@ -79,54 +114,33 @@
     </div>
   </div>
 
-
-  <!-- modal -->
-  <div id="popup-modal"
-    class="fixed  top-0 hidden items-center justify-center  z-50  p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full h-[100vh] ">
-    <div class="relative w-full h-full max-w-md md:h-auto mx-auto ">
-      <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-
-        <div class="p-6 text-center">
-          <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none"
-            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-              d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-          </svg>
-          <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Haqiqatdan ham bu obunachini o'chirmoqchimisiz?</h3>
-          <button id="agree"
-            class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-            Ha,men roziman
-          </button>
-          <button id="cancel"
-            class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Yoq,
-            Bekor qilish</button>
-        </div>
-      </div>
-    </div>
-  </div>
-
   <div class="mt-8"></div>
-  <!-- <pre>{{ users }}</pre> -->
   <div class="flex flex-col mt-8">
     <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-      <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+      <div
+        class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200"
+      >
         <table class="min-w-full">
           <thead>
             <tr>
               <th
-                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+              >
                 Ismi
               </th>
               <th
-                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+              >
                 Obuna vaqti
               </th>
               <th
-                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+              >
                 Status
               </th>
               <th
-                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider"
+              >
                 Role
               </th>
               <th class="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
@@ -134,107 +148,132 @@
           </thead>
 
           <tbody class="bg-white">
-            <tr v-for="item, index in users" :key="index">
+            <tr v-for="(item, index) in users" :key="index">
               <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                 <div class="flex items-center">
                   <div class="flex-shrink-0 h-10 w-10">
-                    <img class="h-10 w-10 rounded-full"
+                    <img
+                      class="h-10 w-10 rounded-full"
                       src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt="" />
+                      alt=""
+                    />
                   </div>
 
                   <div class="ml-4">
-                    <div class="text-sm leading-5 font-medium text-gray-900">{{ item?.username }}</div>
-                    <div class="text-sm leading-5 text-gray-500">{{ item?.email }}</div>
+                    <div class="text-sm leading-5 font-medium text-gray-900">
+                      {{ item?.username }}
+                    </div>
+                    <div class="text-sm leading-5 text-gray-500">
+                      {{ item?.email }}
+                    </div>
                   </div>
                 </div>
               </td>
 
-
               <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                <div class="text-sm leading-5 text-gray-900">{{ formatDate(item?.createdAt) }}</div>
-
+                <div class="text-sm leading-5 text-gray-900">
+                  {{ formatDate(item?.createdAt) }}
+                </div>
               </td>
 
               <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
                 <span
-                  class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">{{
-                    item?.activ? 'Active': 'No active'
-                  }}</span>
+                  class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
+                  >{{ item?.activ ? "Active" : "No active" }}</span
+                >
               </td>
 
-              <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">
+              <td
+                class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500"
+              >
                 {{ item?.role }}
               </td>
 
               <td
-                class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+                class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium"
+              >
                 <button
                   class="bg-[#DC3545] text-white px-4 py-2 rounded-[8px] transition-all duration-300 hover:bg-[red]"
-                  @click="deleteUser(item.id)">O'chirish</button>
+                  @click="deleteUser(item.id)"
+                >
+                  O'chirish
+                </button>
+                <DeleteModal
+                  :is-open="openModal"
+                  @delete="fetchDelete"
+                  @closeModal="openModal = $event"
+                />
               </td>
             </tr>
-
           </tbody>
         </table>
       </div>
     </div>
   </div>
-
-
-
 </template>
 
 <script setup>
 import { onMounted, ref } from "@vue/runtime-core";
+import DeleteModal from "../modal/DeleteModal.vue";
 import axios from "axios";
 import { useToast } from "vue-toastification";
-import formatDate from "../../helpers/formatDate"
-const toast = useToast()
+import formatDate from "../../helpers/formatDate";
+const toast = useToast();
 
-const users = ref(null)
+const openModal = ref(false);
+const deleteId = ref(null);
+
+const users = ref(null);
 
 const fetchUser = () => {
   axios({
-    methods: 'get',
-    url: 'users',
+    methods: "get",
+    url: "users",
     headers: {
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjcyMjkyODk5LCJleHAiOjE2NzIzNzkyOTl9.IZZRWhoRz_57p0aVOZQgA3YqBISHOSVSLHmqMDsr - WU'
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjcyMjkyODk5LCJleHAiOjE2NzIzNzkyOTl9.IZZRWhoRz_57p0aVOZQgA3YqBISHOSVSLHmqMDsr - WU",
     },
     withCredentials: true,
-  }).then((res) => users.value = res.data.data.data)
+  }).then((res) => (users.value = res.data.data.data));
+};
+
+function fetchDelete() {
+  const params = {
+    headers: {},
+    withCredentials: true,
+  };
+  axios
+    .delete(`users/${deleteId.value}`, { params })
+    .then((res) => {
+      fetchUser();
+      toast.success("Muvaffaqiyatli o'chirildi");
+    })
+    .catch((err) => {
+      toast.error("Xatolik yuz berdi");
+    });
 }
 
 const deleteUser = (id) => {
-  const modal = document.getElementById("popup-modal");
-  const agree = document.getElementById("agree");
-  const cancel = document.getElementById("cancel")
-  modal.classList.add('modal-class')
+  openModal.value = true;
+  deleteId.value = id;
+};
 
-  cancel.addEventListener("click", () => {
-    modal.classList.remove('modal-class')
-  })
-
-  const reallyDelete = () => {
-    modal.classList.remove('modal-class')
-    const params = {
-      headers: {},
-      withCredentials: true,
-    }
-    axios.delete(`users/${id}`,{params}).then((res) => {
-      console.log(res)
-      toast.success(res.data.message)
-    })
-  }
-
-  agree.addEventListener("click", reallyDelete)
-
-}
+// fetch products count
+const productsList = ref([]);
+const fetchProductsList = () => {
+  axios({
+    method: "GET",
+    url: "/products",
+    withCredentials: true,
+  }).then((res) => {
+    productsList.value = res.data.data.products;
+  });
+};
 
 onMounted(() => {
-  fetchUser()
-})
-
+  fetchUser();
+  fetchProductsList();
+});
 </script>
 
 <style scoped>
