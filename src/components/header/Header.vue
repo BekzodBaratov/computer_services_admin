1ddb052adadf836cc1cf2f05dd2219fe09bc70c8
Valisher Botirov <valisherbotirov1218@gmail.com>
2023-05-12 15:06:46 +0500

    fix: header

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,8 +1,18 @@
 <template>
-  <header class="flex items-center justify-between px-6 py-4 bg-white border-b-4 border-indigo-600">
+  <header
+    class="flex items-center justify-between px-6 py-4 bg-white border-b-4 border-indigo-600"
+  >
     <div class="flex items-center">
-      <button @click="emit('openSidebar2')" class="text-gray-500 focus:outline-none lg:hidden">
-        <svg class="w-6 h-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <button
+        @click="emit('openSidebar2')"
+        class="text-gray-500 focus:outline-none lg:hidden"
+      >
+        <svg
+          class="w-6 h-6"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
           <path
             d="M4 6H20M4 12H20M4 18H11"
             stroke="currentColor"
@@ -114,7 +124,7 @@
         </div>
       </div> -->
 
-      <div class="profile relative">
+      <!-- <div class="profile relative">
         <button
           @click="dropdownOpen = !dropdownOpen"
           class="relative block w-8 h-8 overflow-hidden rounded-full bg-gray-700 shadow focus:outline-none"
@@ -136,7 +146,7 @@
             >Logout</RouterLink
           >
         </div>
-      </div>
+      </div> -->
     </div>
   </header>
 </template>
